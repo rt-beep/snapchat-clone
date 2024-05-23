517735a53a2f8d837be8444682f7c2cdddd6796b
rt-beep <151120982+rt-beep@users.noreply.github.com>
2024-05-23 14:30:59 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6QQJA-oliz-MmMKBen31eTsraxIL-YUM",
-  authDomain: "snapchat-clone-292f2.firebaseapp.com",
-  projectId: "snapchat-clone-292f2",
-  storageBucket: "snapchat-clone-292f2.appspot.com",
-  messagingSenderId: "40052983260",
-  appId: "1:40052983260:web:ad41b1af9211b2dace7b19"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
